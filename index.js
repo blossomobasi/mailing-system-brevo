1b8bdb94db82aea95b8d4e7193c6d6df1d315415
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-01 21:51:07 +0100

    Set up dotenv for environment variables and add user routes

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+const path = require("path");
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/userRoutes");
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
+dotenv.config({ path: "./config.env" });
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
 	res.json({ message: "Up and running" });
 });
+
+app.use("/api/users", userRoutes);
 
 // Kick off the server
 app.listen(3000, () => {
