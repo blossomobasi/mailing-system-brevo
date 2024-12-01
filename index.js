bce033763191a2e41c523eca3aef3da07667b4be
Blossom Obasi <blossomobasi2@gmail.com>
2024-12-01 18:23:23 +0100

    Set pug view engine

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
 	res.json({ message: "Up and running" });
 });
